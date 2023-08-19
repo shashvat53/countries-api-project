6c96e1317f00f906d6185aff45ad512407797b54
shashvat sen <shashvatsen00@gmail.com>
2023-08-19 19:45:56 +0530

    first commit

--- country.js
+++ country.js
@@ -10,11 +10,13 @@ const capital = document.querySelector(".capital");
 const topLevelDomain = document.querySelector(".topLevelDomain");
 const currencies = document.querySelector(".currencies");
 const languages = document.querySelector(".languages");
+const borders = document.querySelector(".border-contries");
 
 fetch(`https://restcountries.com/v3.1/name/${countryName}?fullText=true`)
   .then((res) => res.json())
   .then((country) => {
     console.log(country);
+
     flag.src = country[0].flags.svg;
     countryHeading.innerText = country[0].name.common;
     if (country[0].name.nativeName) {
@@ -24,7 +26,9 @@ fetch(`https://restcountries.com/v3.1/name/${countryName}?fullText=true`)
     }
     population.innerText = country[0].population.toLocaleString("en-IN");
     region.innerText = country[0].region;
-    subRegion.innerText = country[0].subregion;
+    if (country[0].subregion) {
+      subRegion.innerText = country[0].subregion;
+    }
 
     if (country[0].capital) {
       capital.innerText = country[0].capital.join(", ");
@@ -39,5 +43,20 @@ fetch(`https://restcountries.com/v3.1/name/${countryName}?fullText=true`)
     }
     if (country[0].languages) {
       languages.innerText = Object.values(country[0].languages).join(", ");
+    }
+    if (country[0].borders) {
+      country[0].borders.forEach((border) => {
+        // console.log(border);
+        fetch(`https://restcountries.com/v3.1/alpha/${border}`)
+          .then((res) => res.json())
+          .then(([data]) => {
+            // console.log(data);
+            const borderTag = document.createElement("a");
+            borderTag.innerText = data.name.common;
+            borderTag.href = `/country.html?name=${data.name.common}`;
+            // console.log(borderTag);
+            borders.append(borderTag);
+          });
+      });
     }
   });
--- script.js
+++ script.js
@@ -1,27 +1,48 @@
 const container = document.querySelector(".country-container");
 // console.log(container);
+const filterByRegion = document.querySelector(".filter-by-region");
+// console.log(filterByRegion);
+const searchInput = document.querySelector(".search-container input");
+let allCountriesData;
+
 fetch("https://restcountries.com/v3.1/all")
   .then((res) => res.json())
   .then((data) => {
-    console.log(data.length);
-    data.forEach((country) => {
-      // console.log(country);
-
-      const countryCard = document.createElement("a");
-      countryCard.classList.add("country-card");
-      countryCard.href = `/country.html?name=${country.name.common}`;
-      //   console.log(countryCard);
-      const cardHTML = `
-            <img src=${country.flags.svg} alt="flag" />
-            <div class="card-text">
-              <h2>${country.name.common}</h2>
-              <p><b>population: </b>${country.population.toLocaleString(
-                "en-IN"
-              )}</p>
-              <p><b>Region: </b>${country.region}</p>
-              <p><b>Capital: </b>${country.capital}</p>
-      `;
-      countryCard.innerHTML = cardHTML;
-      container.append(countryCard);
-    });
+    renderCountries(data);
+    allCountriesData = data;
   });
+
+filterByRegion.addEventListener("change", (e) => {
+  fetch(`https://restcountries.com/v3.1/region/${filterByRegion.value}`)
+    .then((res) => res.json())
+    .then(renderCountries);
+});
+
+function renderCountries(data) {
+  container.innerHTML = "";
+  data.forEach((country) => {
+    // console.log(country);
+
+    const countryCard = document.createElement("a");
+    countryCard.classList.add("country-card");
+    countryCard.href = `/country.html?name=${country.name.common}`;
+    //   console.log(countryCard);
+    const cardHTML = `
+          <img src=${country.flags.svg} alt="flag" />
+          <div class="card-text">
+            <h2>${country.name.common}</h2>
+            <p><b>population: </b>${country.population.toLocaleString(
+              "en-IN"
+            )}</p>
+            <p><b>Region: </b>${country.region}</p>
+            <p><b>Capital: </b>${country.capital}</p>
+    `;
+    countryCard.innerHTML = cardHTML;
+    container.append(countryCard);
+  });
+}
+
+searchInput.addEventListener("input", (e) => {
+  console.log(e.target.value);
+  console.log(allCountriesData);
+});
